fc1ce05abbff40ef516bb69935e492db9b80ddc8
Linox <macove.polo@gmail.com>
2023-07-17 15:35:58 -0500

    deploy prod

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 export default defineConfig(({ mode }) => ({
-  // base: mode === 'production' ? '/una-surveys' : '/',
+  base: mode === 'production' ? '/caja-postulantes/dist' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
