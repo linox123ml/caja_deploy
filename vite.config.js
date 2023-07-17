2acf59d54dbf551aa287d7a3a6e2371c223b6e30
Linox <macove.polo@gmail.com>
2023-07-17 15:44:16 -0500

    cambio ruta base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/caja-postulantes/dist' : '/',
+  base: mode === 'production' ? '/pago_admision/dist' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
