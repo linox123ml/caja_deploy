e76b981753d14bdc0076eec47d1d737de7972c29
Linox <macove.polo@gmail.com>
2023-07-18 09:08:52 -0500

    configuracion base de datos

--- server/core/cn.php
+++ server/core/cn.php
@@ -1,5 +1,6 @@
 <?php
-	$cn = new mysqli('localhost', 'root', '', 'netcaja');
+	// $cn = new mysqli('localhost', 'root', '', 'netcaja');
+	$cn = new mysqli('64.20.56.124', 'lino', 'caja2023lnx', 'netcaja');
 	// $cn = new mysqli('localhost', 'root', 'Universidades', 'netcaja');
 	// $cn = new mysqli('137.184.129.66', 'elvis', 'TiYsLawxrF', 'caja');
 	
