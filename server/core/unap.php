e14197a7a4f90c6759af57f360e2012334b20703
Lino Puma Ticona <nearlino20@gmail.com>
2023-07-31 10:15:15 -0500

    test4

--- server/core/unap.php
+++ server/core/unap.php
@@ -12,7 +12,3 @@ $unap = new mysqli($HOST, $USER, $PASSWORD, $NAME);
 if ($unap->connect_errno) {
 	echo "Fallo al conectar a MySQL: (" . $unap->connect_errno . ") " . $unap->connect_error;
 }
-else{
-	echo "Conectado a unap";
-
-}
