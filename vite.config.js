39c20d2065947bec4b36f24ef8e4860118670e01
Linox <macove.polo@gmail.com>
2023-08-03 10:04:15 -0500

    Servidor test

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/caja/pago_admision/dist" : "/",
-  // base: mode === "production" ? "/pago_admision/dist" : "/",
-  
+  // base: mode === "production" ? "/pago_admision/dist" : "/",  
   plugins: [vue()],
   resolve: {
     alias: {
